be4e187a737aee4362f16e355346fcf0ffb4c19e
maroo <mgoothan.psnl@gmail.com>
2022-12-26 11:47:07 +0630

    float64 mul in golang

--- cmd/10/main.go
+++ cmd/10/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math"
+	"strconv"
 )
 
 func main() {
-	layoutISO := "2006-01-02"
-	tt := time.Now()
-	fmt.Println( tt.Format(layoutISO))
-	//t, _ := time.Parse(layoutISO, )
-	//createdAt := fmt.Sprintf("date(created_at) = date(\"%v\")", t.Format(layoutISO))
+	//layoutISO := "2006-01-02"
+	//tt := time.Now()
+	//fmt.Println( tt.Format(layoutISO))
+	////t, _ := time.Parse(layoutISO, )
+	////createdAt := fmt.Sprintf("date(created_at) = date(\"%v\")", t.Format(layoutISO))
 
+	tt := 0.012
+	fmt.Println(100 * tt)
+
+	i := fmt.Sprintf("%.2f", (tt * 100))
+	f, _ := strconv.ParseFloat(i, 2)
+	fmt.Println(f)
+
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
 }
