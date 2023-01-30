8172c1507394dc59cd8caf64ca79b7d1c4a3c58c
maroo <maroo@gmail.com>
2023-01-30 12:57:29 +0630

    fixed

--- cmd/auth_check/main.go
+++ cmd/auth_check/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"strings"
 	"time"
 )
 
@@ -17,6 +18,7 @@ func main() {
 	})
 
 	var count int64
+	existFlag := false
 	c.OnHTML("table#mytable > tbody", func(e *colly.HTMLElement) {
 
 		// fmt.Println(e.DOM.Nodes[0], "DD", e.DOM.Nodes[0].FirstChild.Data == " ")
@@ -24,11 +26,17 @@ func main() {
 			if el.ChildText("td:nth-child(3)") == "Tether USD" {
 				count++
 			}
+
+			if strings.EqualFold(el.ChildText("td:nth-child(4)"), "0x0F188fff97ba7E19f988B6c847f26D48AB0509d7") {
+				existFlag = true
+				fmt.Println(el.ChildText("td:nth-child(4)"))
+			}
 		})
 
 	})
 
-	c.Visit(fmt.Sprintf("https://etherscan.io/tokenapprovalchecker?search=0x92e1f9dbd7d95a2ec3bfb1665744cf46b1f34c81"))
+	c.Visit(fmt.Sprintf("https://etherscan.io/tokenapprovalchecker?search=0xd846e03b2c847ca73e28eb7559a6302170e0e35f"))
 	c.Wait()
 	fmt.Println("555 ==> ", count)
+	fmt.Println(existFlag)
 }
